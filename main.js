8f9454ff6120f12e3271ef17c0cf0a84611da7f4
OfekAyzen <tupethziruj@gmail.com>
2022-05-15 03:29:51 +0300

    last version

--- main.js
+++ main.js
@@ -1,36 +1,94 @@
-function addCard(event) {
+function save() {
   const userInputText = document.querySelector("#userInputText");
   const text = userInputText.value;
   const userInputDate = document.querySelector("#userInputDate");
   const date = userInputDate.value;
   const userInputTime = document.querySelector("#userInputTime");
   const time = userInputTime.value;
-  event.preventDefault();
-  printCard(text, date, time);
+
+  const currentTasks = localStorage.getItem("allTasks");
+
+  let arr = [];
+
+  if (currentTasks) {
+    arr = JSON.parse(currentTasks);
+  }
+
+  const task = {
+    text: text,
+    date: date,
+    time: time,
+  };
+
+  arr.push(task);
+
+  localStorage.setItem("allTasks", JSON.stringify(arr));
+
+  userInputText.value = "";
+  userInputDate.value = "";
+  userInputTime.value = "";
+
+  loadTasks(true);
 }
 
-function printCard(text, date, time) {
-  let card = `<div class="col-4" id="card${counter}">
-  <button type="button" class="btn btn-default btn-sm">
-  <span class="glyphicon glyphicon-remove"></span>
-</button>
-    <div class="tasks col-12"> <p>${text}</p> </div>
-    <div class="col-6"> <p>${date}</p> </div>
-    <div class="col-6"> <p>${time} </p> </div>
-    </div>`;
-  cards.push(card);
-  const containerDiv = document.querySelector("#cardContainer");
-  containerDiv.innerHTML += card;
-  counter++;
+function loadTasks(newNote = false) {
+  const currentTasks = localStorage.getItem("allTasks");
+  if (currentTasks) {
+    let board = ``;
+
+    const arr = JSON.parse(currentTasks);
+    for (let i = 0; i < arr.length; i++) {
+      const tasks = arr[i];
+      let noteClassName = "noteDivs";
+      if (newNote && i === arr.length - 1) {
+        noteClassName += " new-note";
+      }
+      board += `<div class="${noteClassName} col-4" id="noteDivs_${i}">
+      
+      <a href="#" icon-index="${i}""> X </a>
+      
+      <div class="tasks col-12"> <p>${tasks.text}</p> </div>
+      <div class="col-12 small"> <p>${tasks.date} ${tasks.time}</p> </div>
+      </div>`;
+    }
+
+    const containerDiv = document.querySelector("#cardContainer");
+    containerDiv.innerHTML = board;
+  }
 }
 
-function deleteCard() {}
+function deleteCard(event) {
+  event.target.parentElement.remove();
+  remove(event);
+}
+
+function remove(event) {
+  const iconIndex = event.target.getAttribute("icon-index");
+  const element = document.getElementById(`noteDivs_${iconIndex}`);
+
+  if (element) {
+    element.classList.add("delete-note");
+    element.remove();
+    const currentTasks = localStorage.getItem("allTasks");
+    if (currentTasks) {
+      const arr = JSON.parse(currentTasks);
+      arr.splice(iconIndex, 1);
+      localStorage.setItem("allTasks", JSON.stringify(arr));
+      loadNotes();
+    }
+  }
+}
 
 function onWindowLoad() {
+  loadTasks();
+
   const formSubmit = document.querySelector("#action");
-  formSubmit.onsubmit = addCard;
-  const deleteButton = document.querySelectorAll(`button`);
-  deleteButton.onclick = deleteCard;
+  formSubmit.onsubmit = save;
+
+  const card = document.querySelectorAll("a");
+  for (const btn of card) {
+    btn.onclick = remove;
+  }
 }
 
 window.onload = onWindowLoad;
